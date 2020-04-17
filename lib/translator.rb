1f157cccfe1555a9893b4818ac2d85ca64e145fb
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 20:32:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,6 @@ def load_library(file)
   emoticon_hash = {}
   loaded_hash = YAML.load_file("./lib/emoticons.yml")
   loaded_hash.each do |name_of_emoticon, emoticons_arr|
-
     if !emoticon_hash[name_of_emoticon]
       emoticon_hash[name_of_emoticon] = {}
     end

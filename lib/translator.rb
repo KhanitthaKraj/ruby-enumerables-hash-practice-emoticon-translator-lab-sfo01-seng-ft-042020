26d7b216b0536c39eeb150287b87e7eeacacfe24
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 20:35:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,9 +8,6 @@ def load_library(file)
     if !emoticon_hash[name_of_emoticon]
       emoticon_hash[name_of_emoticon] = {}
     end
-    if !emoticon_hash[name_of_emoticon][emoticons_arr]
-      emoticon_hash[name_of_emoticon][emoticons_arr] = {}
-    end
     binding.pry
   end
     

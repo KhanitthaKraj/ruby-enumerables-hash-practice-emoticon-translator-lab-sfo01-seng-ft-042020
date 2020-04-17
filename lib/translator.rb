73a4d07be28e756a6fa8ffe8097fe8129cfbbf34
Khanittha Kraj <kkraj.swe@gmail.com>
2020-04-17 21:55:46 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ end
 
 
 def get_english_meaning(file, japanese_emoticon)
-  load_library(file)
+  load_library("./lib/emoticons.yml")
   emoticon_name = ""
   load_library(file).each do |name, value|
     value.each do |element|
